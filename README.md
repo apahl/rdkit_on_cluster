38de7e2bef01c7296666dc8d89fb6882b2d41f41
Axel Pahl <axelpahl@gmx.de>
2017-09-26 16:57:03 +0200

    first version

---
+++ README.md
@@ -0,0 +1,2 @@
+# RDKit on the Cluster
+A set of tools for working with the [RDKit](http://rdkit.org/) in a distributed way on a cluster.
---
+++ rdkit_on_cluster/rdkit_sss.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+###################
+Substructure Search
+###################
+
+*Created on Thu Aug 17 15:25 2017 by A. Pahl*
+
+Perform substructure search on the cluster."""
+
+
+import argparse
+import sys
+import gzip
+import os.path as op
+from collections import Counter
+
+from tabulate import tabulate
+
+from rdkit.Chem import AllChem as Chem
+
+# imports for similarity search
+# import rdkit.Chem.Scaffolds.MurckoScaffold as MurckoScaffold
+
+
+def usage():
+    print("""Substructure search from multiple Smiles in a tsv file.
+    rdkit_sss.py file_to_search file_w_smiles output_dir job_index""")
+    sys.exit(1)
+
+
+def get_value(str_val):
+    if not str_val:
+        return None
+    if isinstance(str_val, list):
+        print(str_val)
+    try:
+        val = float(str_val)
+        if "." not in str_val:
+            val = int(val)
+    except ValueError:
+        val = str_val
+    return val
+
+
+def write_rec(f, header, rec):
+    line = []
+    for h in header:
+        line.append(rec.get(h, ""))
+    f.write("\t".join(line) + "\n")
+
+
+def rec_from_cells(header, cells):
+    result = {}
+    for idx, h in enumerate(header):
+        if cells[idx] != "":
+            result[h] = cells[idx]
+    return result
+
+
+def iter_csv_mols(fn, header, smiles_col="Canonical_Smiles", max_records=0, tag=True, sep="\t"):
+    """Iterate over the mols in a CSV file"""
+    if not isinstance(fn, list):
+        fn = [fn]
+    rec_counter = 0
+    for filen in fn:
+        if ".gz" in filen:
+            f = gzip.open(filen, mode="rt")
+        else:
+            f = open(filen)
+        first_line = True
+        for line in f:
+            line = line.strip("\n")
+            cells = line.split(sep)
+            if first_line:
+                first_line = False
+                header.extend(cells)
+                if len(fn) > 1 and tag:
+                    header.append("tag")
+                continue
+            # make a copy with non-empty values
+
+            rec = rec_from_cells(header, cells)  # make a copy with non-empty values
+            if smiles_col in rec:
+                mol = Chem.MolFromSmiles(rec[smiles_col])
+                if not mol:
+                    status["Could not generate mol"] += 1
+                    continue
+            else:
+                status["Column {} not found".format(smiles_col)] += 1
+            rec["mol"] = mol
+            rec_counter += 1
+            if max_records > 0 and rec_counter > max_records: break
+            if len(fn) > 1 and tag:
+                rec["tag"] = filen
+            yield rec
+        f.close()
+
+
+def sss_from_file(csv_fn, smiles_fn, output_folder, result_idx, smiles_col="Canonical_Smiles"):
+    """Perform a Smiles substructure search.
+    The list of Smiles and the CSV file to be searched are loaded from file."""
+    smiles_list_orig = open(smiles_fn).read().split("\n")
+    smiles_list_orig = [smi.strip() for smi in smiles_list_orig]
+    status["Smiles read in"] = len(smiles_list_orig)
+    smiles_list = []
+    query_mols = []
+    for smi in smiles_list_orig:
+        mol = Chem.MolFromSmiles(smi)
+        if mol:
+            smiles_list.append(smi)
+            query_mols.append(mol)
+        else:
+            status["Could not generate query mol"] += 1
+    res_fn = op.join(output_folder, "sss_result-{:03}.tsv".format(result_idx))
+    result_file = open(res_fn, "w")
+    first_line = True
+    header = []
+    hit_ctr = 0
+    for rec in iter_csv_mols(csv_fn, header, smiles_col=smiles_col):
+        if first_line:
+            first_line = False
+            header.append("Query")
+            result_file.write("\t".join(header) + "\n")
+            continue
+        mol = rec["mol"]
+        for idx, qm in enumerate(query_mols):
+            if mol.HasSubstructMatch(qm):
+                rec["Query"] = smiles_list[idx]
+                hit_ctr += 1
+                write_rec(result_file, header, rec)
+                break
+    result_file.close()
+    status["Substruct hits"] += 1
+
+
+if __name__ == "__main__":
+    status = Counter()
+    # file_to_search file_w_smiles output_dir job_index
+    parser = argparse.ArgumentParser(
+        descripiton="Substructure search from multiple Smiles in a tsv file.")
+    parser.add_argument("file_to_search", help="the tsv file to be searched.")
+    parser.add_argument("file_w_smiles", help="a file with Smiles to be aearched, one Smiles per line.")
+    parser.add_argument("output_dir", help="where to put the result files.")
+    parser.add_argument("job_idx", help="the job index (used for the name of the result file.",
+                        type=int)
+    parser.add_argument("-s", "--smiles", help="name of the Smiles column in the tsv file.")
+    # parser.add_argument("", help="")
+
+    args = parser.parse_args()
+    smiles_col = "Canonical_Smiles"
+    if args.smiles is not None:
+        smiles_col = args.smiles
+    print(smiles_col)
+    err = False
+    reason = ""
+    if not op.isfile(args.file_to_search):
+        err = True
+        reason = "Input file {} could not be found.".format(args.file_to_search)
+    elif not op.isfile(args.file_w_smiles):
+        err = True
+        reason = "File with Smiles {} could not be found.".format(args.file_w_smiles)
+    elif not op.isdir(args.output_dir):
+        err = True
+        reason = "Output directory {} does not exist.".format(args.output_dir)
+
+    if err:
+        print(reason)
+        usage()
+
+    sss_from_file(args.file_to_search, args.file_w_smiles, args.output_dir,
+                  args.job_idx, smiles_col)
+
+    print(tabulate(status.items()))
